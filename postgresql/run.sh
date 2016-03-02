cc0f6dc3a2f3e377554893eb1d5209d0ee939db8
Daniel Keler <danielk@jfrog.com>
2016-03-02 12:29:03 +0200

    Added docker-compose for each combination and improved generic

--- postgresql/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-createArtifactoryUser(){
-    psql -c "CREATE USER artifactory WITH PASSWORD 'password';CREATE DATABASE artifactory WITH OWNER=artifactory ENCODING='UTF8';GRANT ALL PRIVILEGES ON DATABASE artifactory TO artifactory;"
-}
-
-#createArtifactoryUser
